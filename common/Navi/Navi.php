6216673729f7a0057e586d9e5e36c4c1b4b11f4b
Gavin Li <craftgavin@gmail.com>
2012-05-17 10:28:36 +0800

    a

--- common/Navi/Navi.php
+++ common/Navi/Navi.php
@@ -8,9 +8,13 @@ class Navi extends App_Brick_Fixed_Abstract
 		if(array_key_exists('left', $naviArr)) {
 			$this->_naviArr = $naviArr;
 		} else {
+			$csu = Class_Session_User::getInstance();
 			$this->_naviArr = array(
 				'left' => $naviArr,
-				'right' => array()
+				'right' => array(
+					array('title' => '我的账户', 'url' => '/'.$csu->getUserId().'/user'),
+					array('title' => '登出', 'url' => 'http://sso.enorange.cn/sso/logout')
+				)
 			);
 		}
 	}
